a4672951f1d970e0f3457e8b7f545951e9a93228
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-04-27 22:53:42 -0300

    feat: sum itens

--- public/app/app.js
+++ public/app/app.js
@@ -1,2 +1,16 @@
+import { handleStatus, log } from './utils/promiseHelpers.js'
+import './utils/arrayUtils.js'
+
+const sumItems = code => notes => notes
+  .$flatMap(arr => arr.itens)
+  .filter(item => item.codigo === code)
+  .reduce((total, item) => total + item.valor, 0)
+
 document.querySelector('#myButton')
-  .addEventListener('click', () => alert('hello'))
+  .addEventListener('click', async () => {
+    fetch('/notas')
+      .then(handleStatus)
+      .then(sumItems('2143'))
+      .then(log)
+      .catch(log)
+  })
