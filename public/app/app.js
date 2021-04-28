07ef1cdbd5e0702f94ee0135b191f457da2ee160
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-04-27 23:17:24 -0300

    refactor: transfer responsability

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,12 @@ import { noteService as service } from './services/noteService.js'
 import { log } from './utils/promiseHelpers.js'
 import './utils/arrayUtils.js'
 
-const sumItems = code => notes => notes
-  .$flatMap(arr => arr.itens)
-  .filter(item => item.codigo === code)
-  .reduce((total, item) => total + item.valor, 0)
+
 
 document.querySelector('#myButton')
   .onclick = () =>
     service
-    .listAll()
-    .then(sumItems('2143'))
+    .sumItems('2143')
     .then(log)
     .catch(log)
   
--- public/app/services/noteService.js
+++ public/app/services/noteService.js
@@ -2,8 +2,16 @@ import { handleStatus } from '../utils/promiseHelpers.js'
 
 const api = '/notas'
 
+const sumItems = code => notes => notes
+  .$flatMap(arr => arr.itens)
+  .filter(item => item.codigo === code)
+  .reduce((total, item) => total + item.valor, 0)
+
 export const noteService = {
   listAll () {
     return fetch(api).then(handleStatus)
+  },
+  sumItems (code) {
+    return this.listAll().then(sumItems(code))
   }
 }
